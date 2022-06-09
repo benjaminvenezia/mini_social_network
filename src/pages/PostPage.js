9f5a837008a23b6a1c74b70f7ec91c5dc45080fd
Benjamin Venezia <benjamin.venezia@crea-inseec.com>
2022-06-09 19:00:10 +0200

    last commit

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,5 +1,7 @@
 import { FormPost } from "../components";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Wrapper = styled.div`
   width: 80%;
@@ -8,6 +10,8 @@ const Wrapper = styled.div`
 `;
 
 const PostPage = () => {
+  const navigate = useNavigate();
+
   return (
     <Wrapper>
       <h1>Post</h1>
